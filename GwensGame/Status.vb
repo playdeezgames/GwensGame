34537b6c97de71f872a110093a031ea86ecfead3
ernestpazera <ernestpazera@gmail.com>
2021-10-21 07:27:03 -0500

    0,0 is yer house

--- GwensGame/Status.vb
+++ GwensGame/Status.vb
@@ -1,10 +1,32 @@
 ﻿Module Status
+    Public ShowHouse As Action =
+        Sub()
+            AddPrompt("Yer at yer house!")
+        End Sub
+    Sub DefaultStatus()
+        AddPrompt($"You are at ({Data.GetX()}, {Data.GetY()}) and facing {GetDirectionName(Data.GetFacing())}.")
+    End Sub
+    Dim locations As New Dictionary(Of Integer, Dictionary(Of Integer, Action)) From
+        {
+            {0, New Dictionary(Of Integer, Action) From {
+                {0, ShowHouse}
+            }}
+        }
+    Sub ShowSpecificStatus()
+        If locations.ContainsKey(GetX()) Then
+            If locations(GetX()).ContainsKey(GetY()) Then
+                locations(GetX())(GetY())()
+                Return
+            End If
+        End If
+        DefaultStatus()
+    End Sub
     Public ShowStatus As Action =
     Sub()
         ClearInputItems()
-        AddPrompt($"You are at ({Data.GetX()}, {Data.GetY()}) and facing {GetDirectionName(Data.GetFacing())}.")
         AddInputItem("Move...", ShowMoveMenu)
         AddInputItem("Turn...", ShowTurnMenu)
+        ShowSpecificStatus()
         AddInputItem("Main Menu", ShowMainMenu)
     End Sub
 End Module
