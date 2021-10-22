49cbcc79ca0ab7039d543789a975126f105141af
ernestpazera <ernestpazera@gmail.com>
2021-10-22 16:22:49 -0500

    moved boilerplat to another module

---
+++ GwensJourney/Boilerplate.vb
@@ -0,0 +1,25 @@
+﻿Module Boilerplate
+    ReadOnly QuitGame As Action =
+        Sub()
+            ClearPrompts()
+            ClearActions()
+        End Sub
+
+    ReadOnly ConfirmQuit As Action =
+        Sub()
+            ClearPrompts()
+            AddPrompt("Are you sure you want to quit?")
+            ClearActions()
+            AddAction("No", MainMenu)
+            AddAction("Yes", QuitGame)
+        End Sub
+
+    ReadOnly MainMenu As Action =
+        Sub()
+            ClearPrompts()
+            AddPrompt("Main Menu:")
+            ClearActions()
+            AddAction("Quit", ConfirmQuit)
+        End Sub
+
+End Module
--- GwensJourney/Program.vb
+++ GwensJourney/Program.vb
@@ -3,28 +3,6 @@ Module Program
         Console.WriteLine("Welcome to Gwen's Journey")
     End Sub
 
-    ReadOnly QuitGame As Action =
-        Sub()
-            ClearPrompts()
-            ClearActions()
-        End Sub
-
-    ReadOnly ConfirmQuit As Action =
-        Sub()
-            ClearPrompts()
-            AddPrompt("Are you sure you want to quit?")
-            ClearActions()
-            AddAction("No", MainMenu)
-            AddAction("Yes", QuitGame)
-        End Sub
-
-    ReadOnly MainMenu As Action =
-        Sub()
-            ClearPrompts()
-            AddPrompt("Main Menu:")
-            ClearActions()
-            AddAction("Quit", ConfirmQuit)
-        End Sub
     Sub MainLoop()
         While HasActions()
             Do
