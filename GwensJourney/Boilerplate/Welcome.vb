ec4c8b1f465d2e68c4ca3e002ee31147f86690c2
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-01 07:47:28 -0500

    welcome

---
+++ GwensJourney/Boilerplate/Welcome.vb
@@ -0,0 +1,8 @@
+﻿Friend Module Welcome
+    Friend Sub Run()
+        AnsiConsole.Write(
+            New FigletText("Welcome to Gwen's Journey") With {
+                .Color = Color.Fuchsia,
+                .Justification = Justify.Center})
+    End Sub
+End Module
--- GwensJourney/Welcome.vb
+++
@@ -1,5 +0,0 @@
-﻿Friend Module Welcome
-    Friend Sub Run()
-        Console.WriteLine("Welcome to Gwen's Journey")
-    End Sub
-End Module
