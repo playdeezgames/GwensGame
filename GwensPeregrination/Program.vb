4cc6aa3ab59814fc78688c5bc04fe15b56922457
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-12 08:21:03 -0500

    clear the screen

--- GwensPeregrination/Program.vb
+++ GwensPeregrination/Program.vb
@@ -2,17 +2,21 @@ Module Program
     Sub Main(args As String())
         Dim config = GPConfig.Load()
         Dim engine As New Engine(config)
-        Dim frameBuffer As IFrameBuffer = New FrameBuffer(80, 25)
-        frameBuffer.ForegroundColor = 12
-        frameBuffer.WriteLine("               ___                 _")
-        frameBuffer.WriteLine("              / __|_ __ _____ _ _ ( )___")
-        frameBuffer.WriteLine("             | (_ \ V  V / -_) ' \|/(_-<")
-        frameBuffer.WriteLine("              \___|\_/\_/\___|_||_| /__/           _   _")
-        frameBuffer.WriteLine("             | _ \___ _ _ ___ __ _ _ _(_)_ _  __ _| |_(_)___ _ _  ")
-        frameBuffer.WriteLine("             |  _/ -_) '_/ -_) _` | '_| | ' \/ _` |  _| / _ \ ' \ ")
+        Dim frameBuffer As IFrameBuffer = New FrameBuffer(80, 25, Gray, Black)
+        frameBuffer.Clear()
+        frameBuffer.ForegroundColor = LightRed
+        frameBuffer.WriteLine("                          ___                 _")
+        frameBuffer.WriteLine("                         / __|_ __ _____ _ _ ( )___")
+        frameBuffer.WriteLine("                        | (_ \ V  V / -_) ' \|/(_-<")
+        frameBuffer.WriteLine("              ___        \___|\_/\_/\___|_||_| /__/_   _")
+        frameBuffer.WriteLine("             | _ \___ _ _ ___ __ _ _ _(_)_ _  __ _| |_(_)___ _ _")
+        frameBuffer.WriteLine("             |  _/ -_) '_/ -_) _` | '_| | ' \/ _` |  _| / _ \ ' \")
         frameBuffer.WriteLine("             |_| \___|_| \___\__, |_| |_|_||_\__,_|\__|_\___/_||_|")
         frameBuffer.WriteLine("                             |___/")
-        frameBuffer.ForegroundColor = 7
+        frameBuffer.ForegroundColor = White
+        frameBuffer.WriteLine("                              Gwen's Peregrination")
+        frameBuffer.ForegroundColor = Gray
+        frameBuffer.WriteLine("                        A Production of TheGrumpyGameDev")
         Using host As New Host(
             "Gwen's Peregrination",
             frameBuffer,
