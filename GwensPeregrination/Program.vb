00115f50dee2565081e2d7a529e84c8e72d9669f
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-12 06:01:32 -0500

    bubble letters!

--- GwensPeregrination/Program.vb
+++ GwensPeregrination/Program.vb
@@ -3,7 +3,16 @@ Module Program
         Dim config = GPConfig.Load()
         Dim engine As New Engine(config)
         Dim frameBuffer As IFrameBuffer = New FrameBuffer(80, 25)
-        frameBuffer.WriteLine("Welcome to Gwen's Peregrination!")
+        frameBuffer.ForegroundColor = 12
+        frameBuffer.WriteLine("               ___                 _")
+        frameBuffer.WriteLine("              / __|_ __ _____ _ _ ( )___")
+        frameBuffer.WriteLine("             | (_ \ V  V / -_) ' \|/(_-<")
+        frameBuffer.WriteLine("              \___|\_/\_/\___|_||_| /__/           _   _")
+        frameBuffer.WriteLine("             | _ \___ _ _ ___ __ _ _ _(_)_ _  __ _| |_(_)___ _ _  ")
+        frameBuffer.WriteLine("             |  _/ -_) '_/ -_) _` | '_| | ' \/ _` |  _| / _ \ ' \ ")
+        frameBuffer.WriteLine("             |_| \___|_| \___\__, |_| |_|_||_\__,_|\__|_\___/_||_|")
+        frameBuffer.WriteLine("                             |___/")
+        frameBuffer.ForegroundColor = 7
         Using host As New Host(
             "Gwen's Peregrination",
             frameBuffer,
