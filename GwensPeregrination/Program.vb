fbcf1e5133d085de30140bf8125d4a32f9bcdff9
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-12 05:37:49 -0500

    title and icon done

--- GwensPeregrination/Program.vb
+++ GwensPeregrination/Program.vb
@@ -2,7 +2,7 @@ Module Program
     Sub Main(args As String())
         Dim engine As New Engine()
         Dim frameBuffer As IFrameBuffer = New FrameBuffer(80, 25)
-        Using host As New Host(FrameBuffer, engine, engine, 1, False)
+        Using host As New Host("Gwen's Peregrination", frameBuffer, engine, engine, 1, False)
             host.Run()
         End Using
     End Sub
