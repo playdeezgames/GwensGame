fd8c50fc8fa65bba3132ec5f3f1e79f92f1b5789
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-13 05:27:13 -0500

    got rid of old publish

--- shippit.sh
+++ shippit.sh
@@ -1,8 +1,8 @@
-dotnet publish ./GwensJourney/GwensJourney.vbproj -o ./pub-linux -c Release --sc -r linux-x64
-dotnet publish ./GwensJourney/GwensJourney.vbproj -o ./pub-windows -c Release --sc -r win-x64
-dotnet publish ./GwensJourney/GwensJourney.vbproj -o ./pub-mac -c Release --sc -r osx-x64
-butler push pub-windows thegrumpygamedev/gwens-journey:windows
-butler push pub-linux thegrumpygamedev/gwens-journey:linux
-butler push pub-mac thegrumpygamedev/gwens-journey:mac
+dotnet publish ./GwensPeregrination/GwensPeregrination.vbproj -o ./pub-linux -c Release --sc -r linux-x64
+dotnet publish ./GwensPeregrination/GwensPeregrination.vbproj -o ./pub-windows -c Release --sc -r win-x64
+dotnet publish ./GwensPeregrination/GwensPeregrination.vbproj -o ./pub-mac -c Release --sc -r osx-x64
+butler push pub-windows thegrumpygamedev/gwens-peregrination:windows
+butler push pub-linux thegrumpygamedev/gwens-peregrination:linux
+butler push pub-mac thegrumpygamedev/gwens-peregrination:mac
 git add -A
 git commit -m "shipped it!"
