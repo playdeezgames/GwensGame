335ff2142fd6c8fb748cb980bbff5a6927b10790
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-14 04:46:43 -0500

    i can load a game

--- GP.Application/States/Boilerplate/LoadState.vb
+++ GP.Application/States/Boilerplate/LoadState.vb
@@ -4,7 +4,6 @@
     Public Sub New(stateMachine As IApplicationStateMachine, frameBuffer As IFrameBuffer)
         MyBase.New(stateMachine, frameBuffer)
     End Sub
-
     Public Overrides Sub Run()
         With _frameBuffer
             .BackgroundColor = Black
@@ -12,14 +11,38 @@
             .ForegroundColor = Brown
             .WriteLine("Load From:")
             .ForegroundColor = Gray
+            Dim index = 1
+            For Each slot In SaveSlots
+                If File.Exists(slot.Item2) Then
+                    .WriteLine($"{index}. {slot.Item1}")
+                End If
+                index += 1
+            Next
             .WriteLine("0. Never Mind")
         End With
     End Sub
-
     Protected Overrides Function HandleChoice(choice As Integer) As Boolean
         Select Case choice
             Case 0
                 ChangeToState(MainMenu)
+            Case 1 To 5
+                Dim filename = SaveSlots(choice - 1).Item2
+                If Not File.Exists(filename) Then
+                    Return False
+                End If
+                Engine.World = World.Load(filename)
+                With _frameBuffer
+                    .BackgroundColor = Black
+                    .WriteLine()
+                    If Engine.World IsNot Nothing Then
+                        .ForegroundColor = LightGreen
+                        .WriteLine($"Loaded game from {SaveSlots(choice - 1).Item1}!")
+                        ChangeToState(Neutral)
+                    Else
+                        .ForegroundColor = Red
+                        .WriteLine($"Failed to load game from {SaveSlots(choice - 1).Item1}!")
+                    End If
+                End With
             Case Else
                 Return False
         End Select
