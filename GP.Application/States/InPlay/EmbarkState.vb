b5d0a5b5915e3835f8b7a4b88087a022dbd05f7d
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-14 05:07:47 -0500

    prolog!

--- GP.Application/States/InPlay/EmbarkState.vb
+++ GP.Application/States/InPlay/EmbarkState.vb
@@ -6,6 +6,17 @@
     End Sub
 
     Public Overrides Sub Run()
+        With _frameBuffer
+            .BackgroundColor = Black
+            .WriteLine()
+            .ForegroundColor = LightCyan
+            .WriteLine("Prolog:")
+            .ForegroundColor = Gray
+            .WriteLine("You are Gwen. You seek to get to yer destination, which is quite far away. You")
+            .WriteLine("will get hungry along the way, and you start out with a few snax, but you'll")
+            .WriteLine("need to find more if you hope to make it. You will face various challenges and")
+            .WriteLine("choices along yer way. Good luck!")
+        End With
         Engine.World = World.Create()
         WorldInitializer.Initialize(Engine.World)
         ChangeToState(ApplicationStates.Neutral)
