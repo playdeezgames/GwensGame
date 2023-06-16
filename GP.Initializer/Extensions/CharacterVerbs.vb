f6451be13b9a96d6d68c4adca8049ba77a12bcf1
TheGrumpyGameDev <ernestpazera@gmail.com>
2023-06-16 15:51:32 -0500

    function, not sub

--- GP.Initializer/Extensions/CharacterVerbs.vb
+++ GP.Initializer/Extensions/CharacterVerbs.vb
@@ -1,26 +1,28 @@
 ﻿Imports System.Runtime.CompilerServices
 Friend Module CharacterVerbs
-    Private ReadOnly verbTable As IReadOnlyDictionary(Of String, Action(Of ICharacter, IReadOnlyDictionary(Of String, Integer))) =
-        New Dictionary(Of String, Action(Of ICharacter, IReadOnlyDictionary(Of String, Integer))) From
+    Private ReadOnly verbTable As IReadOnlyDictionary(Of String, Func(Of ICharacter, IReadOnlyDictionary(Of String, Integer), Boolean)) =
+        New Dictionary(Of String, Func(Of ICharacter, IReadOnlyDictionary(Of String, Integer), Boolean)) From
         {
             {VerbType.Move, AddressOf DoMove},
             {VerbType.Eat, AddressOf DoEat},
             {VerbType.Forage, AddressOf DoForage}
         }
     <Extension>
-    Friend Sub DoVerb(character As ICharacter, verb As String, ParamArray parms As (String, Integer)())
-        verbTable(verb)(character, parms.ToDictionary(Function(x) x.Item1, Function(x) x.Item2))
-    End Sub
-    Private Sub DoForage(character As ICharacter, table As IReadOnlyDictionary(Of String, Integer))
+    Friend Function DoVerb(character As ICharacter, verb As String, ParamArray parms As (String, Integer)()) As Boolean
+        Return verbTable(verb)(character, parms.ToDictionary(Function(x) x.Item1, Function(x) x.Item2))
+    End Function
+    Private Function DoForage(character As ICharacter, table As IReadOnlyDictionary(Of String, Integer)) As Boolean
         Throw New NotImplementedException()
-    End Sub
-    Private Sub DoEat(character As ICharacter, table As IReadOnlyDictionary(Of String, Integer))
+    End Function
+    Private Function DoEat(character As ICharacter, table As IReadOnlyDictionary(Of String, Integer)) As Boolean
         character.SetSnax(character.Snax - 1)
         character.SetSatiety(character.Satiety + table(StatisticType.Satiety))
-    End Sub
-    Private Sub DoMove(character As ICharacter, table As IReadOnlyDictionary(Of String, Integer))
+        Return True
+    End Function
+    Private Function DoMove(character As ICharacter, table As IReadOnlyDictionary(Of String, Integer)) As Boolean
         character.SetDistanceRemaining(character.DistanceRemaining - table(StatisticType.Distance))
         ApplyHunger(character, table(StatisticType.Distance))
         RefreshLocalArea(character)
-    End Sub
+        Return True
+    End Function
 End Module
